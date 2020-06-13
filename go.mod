476a489ebe941e6c799cade6cbf676bc31aecfe0
akaimo <kawashu0723@gmail.com>
2020-06-13 22:42:07 +0900

    add v1alpha1/register.go

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/akaimo/job-observer
 go 1.14
 
 require (
+	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/client-go v0.17.0
 	k8s.io/code-generator v0.17.0 // indirect
+	k8s.io/klog v1.0.0
 )
--- pkg/apis/cleaner/v1alpha1/register.go
+++ pkg/apis/cleaner/v1alpha1/register.go
@@ -1,1 +1,34 @@
 package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/akaimo/job-observer/pkg/apis/cleaner"
+)
+
+var SchemeGroupVersion = schema.GroupVersion{Group: cleaner.GroupName, Version: "v1"}
+
+func Kind(kind string) schema.GroupKind {
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
+}
+
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+var (
+	// SchemeBuilder initializes a scheme builder
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	// AddToScheme is a global function that registers this API group & version to a scheme
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Cleaner{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
